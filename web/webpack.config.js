c9c8b39cd4cb6b7f6e1015c3c4f74d444015e6e1
Rocky Assad <rocky@fourq.io>
2017-09-03 19:49:54 -0500

    everything

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,19 +2,23 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config.js');
 
 const isProd = process.env.NODE_ENV === 'production';
 const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 const cssProd = ExtractTextPlugin.extract({
   fallback: 'style-loader',
   use: ['css-loader', 'sass-loader'],
-  publicPath: '/dist'
+  publicPath: 'dist/'
 });
+
 const cssConfig = isProd ? cssProd : cssDev;
+const bootstratConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    bootstrap: bootstratConfig
   },
   output: {
     filename: '[name].bundle.js',
@@ -22,26 +26,32 @@ module.exports = {
   },
   devServer: {
     compress: true,
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'dist'),
     hot: true,
     open: true,
     stats: 'errors-only'
   },
   module: {
     rules: [
-      { 
-        test: /\.js$/, 
-        exclude: /node_modules/, 
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: 'babel-loader'
       },
-      { 
-        test: /\.scss$/, 
+      {
+        test: /\.scss$/,
         use: cssConfig
       },
-      { 
-        test: /\.png$/, 
-        use: 'file-loader'
-      }
+      {
+        test: /\.png$/,
+        use: [
+          'file-loader?name=[name].[ext]?[hash]&publicPath=./&outputPath=images/',
+          'image-webpack-loader'
+        ]
+      },
+      { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000?name=[name].[ext]&publicPath=./&outputPath=fonts/' },
+      { test: /\.(ttf|eot)$/, loader: 'file-loader?name=[name].[ext]&publicPath=./&outputPath=fonts/' },
+      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' }
     ]
   },
   plugins: [
@@ -54,7 +64,7 @@ module.exports = {
       template: './src/index.html'
     }),
     new ExtractTextPlugin({
-      filename: '[name].css',
+      filename: '/css/[name].css',
       disable: !isProd,
       allChunks: true
     }),
